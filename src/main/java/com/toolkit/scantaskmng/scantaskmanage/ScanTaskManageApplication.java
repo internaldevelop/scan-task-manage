088c98906793d98da249577bb95e0d4695c66a53
ytwei <249422@qq.com>
2019-05-08 17:39:59 +0800

    Initial commit

---
+++ src/main/java/com/toolkit/scantaskmng/scantaskmanage/ScanTaskManageApplication.java
@@ -0,0 +1,13 @@
+package com.toolkit.scantaskmng.scantaskmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanTaskManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScanTaskManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toolkit/scantaskmng/scantaskmanage/ScanTaskManageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.toolkit.scantaskmng.scantaskmanage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScanTaskManageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
