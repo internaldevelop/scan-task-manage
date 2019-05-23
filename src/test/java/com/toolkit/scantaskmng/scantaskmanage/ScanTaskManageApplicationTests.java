c08b3a8e1ebe2682a843d94c76b29b70bfbce068
ytwei <249422@qq.com>
2019-05-23 13:42:01 +0800

    Add artifact / packaging.

--- src/test/java/com/toolkit/scantaskmng/scantaskmanage/ScanTaskManageApplicationTests.java
+++ src/test/java/com/toolkit/scantaskmng/scantaskmanage/ScanTaskManageApplicationTests.java
@@ -1,12 +1,14 @@
 package com.toolkit.scantaskmng.scantaskmanage;
 
+import com.toolkit.scantaskmng.ScanTaskManageApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
+@SpringBootTest(classes = {ScanTaskManageApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ScanTaskManageApplicationTests {
 
     @Test
