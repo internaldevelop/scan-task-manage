1527e926ca41d9ffb5660dec1c4a5d2b249c9ff6
lvjz2018 <lvjianzhang@sina.com>
2019-08-09 16:55:41 +0800

    add NetConnectApi

---
+++ src/main/java/com/toolkit/scantaskmng/controller/NetConnectApi.java
@@ -0,0 +1,37 @@
+package com.toolkit.scantaskmng.controller;
+
+import com.toolkit.scantaskmng.global.bean.ResponseBean;
+import com.toolkit.scantaskmng.global.response.ResponseHelper;
+import com.toolkit.scantaskmng.global.utils.SystemUtils;
+import com.toolkit.scantaskmng.service.AssetInfoService;
+import com.toolkit.scantaskmng.service.NetConnectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*",maxAge = 3600)
+@RestController
+@RequestMapping(value="/netconnect")
+public class NetConnectApi {
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private ResponseHelper responseHelper;
+    @Autowired
+    private NetConnectService netconnectService;
+
+    /**
+     *1.1 网络路径连通性测试
+     * @param ip 系统配置名称
+     * @return payload
+     */
+    @RequestMapping(value = "/param", method = RequestMethod.GET)
+    public @ResponseBody
+    Object netConnect(@RequestParam("ip") String ip) {
+        ResponseBean response = netconnectService.ping(ip);
+        return response;
+//        return netconnectService.ping(ip);
+    }
+
+}
