61e73ba64f3b5d3cfb9bb6b50c12d700a5669d64
ytwei <249422@qq.com>
2019-10-14 16:04:04 +0800

    for test github user login

---
+++ lib/sigar/readme.txt
@@ -0,0 +1,2 @@
+dll文件： windows系统，拷贝到c:\windows目录下
+so文件： Linux系统，拷贝到/usr/lib64/目录下
--- src/main/java/com/toolkit/scantaskmng/ScanTaskManageApplication.java
+++ src/main/java/com/toolkit/scantaskmng/ScanTaskManageApplication.java
@@ -3,6 +3,8 @@ package com.toolkit.scantaskmng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// for test github user login
+
 @SpringBootApplication
 public class ScanTaskManageApplication {
 
